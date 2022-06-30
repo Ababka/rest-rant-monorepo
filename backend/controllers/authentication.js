0f609c6c0da3be537634d50b606114dd7a0ae57b
Anthony Babka <anthonybabka@gmail.com>
2022-06-30 00:48:16 -0700

    adding new branch

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
@@ -14,7 +15,8 @@ if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         message: `Could not find a user with the provided username and password` 
     })
 } else {
-    res.json({ user })
+    const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+        res.json({ user: user, token: result.value })
     }
 })
 
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 
 export const CurrentUser = createContext()
@@ -6,12 +6,14 @@ export const CurrentUser = createContext()
 function CurrentUserProvider({ children }){
 
     const [currentUser, setCurrentUser] = useState(null)
-
-    return (
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </CurrentUser.Provider>
-    )
-}
+    useEffect ( () => {
+        const getLoggedInUser = async () => {
+            let response = await fetch('http://localhost:3000/authentication/profile')
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+        }, [])
+    }
 
 export default CurrentUserProvider
